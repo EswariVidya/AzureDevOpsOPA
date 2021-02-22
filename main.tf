c9ee0a4f7d5c39a5542125d8f3076d3e72eb366e
EswariVidya <79461522+EswariVidya@users.noreply.github.com>
2021-02-22 15:27:38 +0100

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,37 @@
+provider "azurerm" {
+    version = "~>2.13.0"
+    subscription_id = var.subscription_id
+    tenant_id = var.tenant_id
+    client_id = var.client_id
+    client_secret = var.client_secret
+    features{}
+}
+
+terraform {
+    backend "azurerm" {}
+}
+
+data "azurerm_client_config" "current" {}
+
+resource "azurerm_resource_group" "rg" {
+    name = "devSecOps"
+    location = "west us"
+} 
+resource "azurerm_network_security_group" "nsg" {
+    name = "myTFNSG"
+    location = "west europe"
+    resource_group_name = azurerm_resource_group.rg.name
+
+    security_rule {
+        name = "SSH"
+        priority = 1001
+        direction = "Inbound"
+        access = "Allow"
+        protocol = "Tcp"
+        source_port_range = "*"
+        destination_port_range = "22"
+        source_address_prefix = "*"
+        destination_address_prefix = "*"
+
+    }
+}
