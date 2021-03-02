998e792c60acd6b24076b526259a2124364e7c78
EswariVidya <79461522+EswariVidya@users.noreply.github.com>
2021-03-02 15:27:31 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,23 +17,23 @@ data "azurerm_client_config" "current" {}
 #    name = "devSecOpsrg"
 #    location = "west us"
 #} 
-resource "azurerm_network_security_group" "nsg" {
-    name = "myTFNSG"
-    location = var.location
-    resource_group_name = var.resource_groupname
-    security_rule {
-        name = "SSH"
-        priority = 1001
-        direction = "Inbound"
-        access = "Allow"
-        protocol = "Tcp"
-        source_port_range = "*"
-        destination_port_range = "22"
-        source_address_prefix = "*"
-        destination_address_prefix = "*"
+#resource "azurerm_network_security_group" "nsg" {
+#    name = "myTFNSG"
+#    location = var.location
+#    resource_group_name = var.resource_groupname
+ #   security_rule {
+ #       name = "SSH"
+ #       priority = 1001
+ #       direction = "Inbound"
+ #       access = "Allow"
+ #       protocol = "Tcp"
+ #       source_port_range = "*"
+ #       destination_port_range = "22"
+ #       source_address_prefix = "*"
+ #       destination_address_prefix = "*"
 
-    }
-}
+  #  }
+#}
 
 resource "azurerm_storage_account" "teststorageaccount1" {
   name                      = "devsecopsstorageaccount"
