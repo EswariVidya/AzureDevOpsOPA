5f0e740bd6217bdab3e3d1c4124d60649414c6f1
EswariVidya <79461522+EswariVidya@users.noreply.github.com>
2021-03-03 12:14:57 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,9 +2,6 @@ variable "subscription_id"{}
 variable "tenant_id"{}
 variable "client_id"{}
 variable "client_secret"{}
-variable "resource_groupname" {
-  default = "<Enter resource group name>"
-}
 variable "location" {
   type = string
   default = "westeurope"
