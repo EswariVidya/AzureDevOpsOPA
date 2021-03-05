595a8918266c2e0971ff307be4eb372b4ebc57d0
EswariVidya <79461522+EswariVidya@users.noreply.github.com>
2021-03-05 16:33:37 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,8 +2,8 @@ variable "subscription_id"{}
 variable "tenant_id"{}
 variable "client_id"{}
 variable "client_secret"{}
-variable "resource_gp"{
-  default = "<Enter resource group name>"
+variable "resource_gp_name"{
+  default = " "
 }
 variable "location" {
   type = string
